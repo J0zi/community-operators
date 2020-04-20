240033b34eb6c787bc71b722157e62d0cec640c4
j0zi <jbreza@redhat.com>
2020-04-20 14:57:51 +0200

    corrected test logic output
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/run-deployment-tests.sh
+++ scripts/ci/run-deployment-tests.sh
@@ -4,22 +4,22 @@ eval $(scripts/ci/operators-env)
 echo "First test: IS_TESTABLE in run-deployment-tests is $IS_TESTABLE"
 if [ -z "${IS_TESTABLE}" ];
 then
+    if [ -z "${OP_PATH}" ] ;
+  then
+    echo "No operator modification detected. Exiting."
+    exit 0
+  else
+    echo "Detected modified Operator in ${OP_PATH}"
+    echo "Detected modified Operator version ${OP_VER}"
+  fi
 
-  echo "IS_TESTABLE in run-deployment-tests is $IS_TESTABLE"
+  make operator.install OP_PATH="${OP_PATH}" OP_VER="${OP_VER}"
+
+else
+    echo "IS_TESTABLE in run-deployment-tests is $IS_TESTABLE"
 
   if [[ $IS_TESTABLE -eq 0 ]]; then
    echo "Nothing was changed, not running the CI."
     exit 0
   fi
-else
-  if [ -z "${OP_PATH}" ] ;
-  then
-    echo "No operator modification detected. Exiting."
-    exit 0 
-  else
-    echo "Detected modified Operator in ${OP_PATH}"
-    echo "Detected modified Operator version ${OP_VER}"
-  fi
-      
-  make operator.install OP_PATH="${OP_PATH}" OP_VER="${OP_VER}"
 fi
